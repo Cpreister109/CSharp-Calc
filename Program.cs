f54c97631a0f66f295df633dc052309f026e3629
Cade Preister <cadepreister@unomaha.edu>
2024-07-17 15:58:58 -0500

    expanding methods

--- Program.cs
+++ Program.cs
@@ -10,97 +10,82 @@ namespace calcshortforcalculator
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome! What would you like to solve?");
-            Calculate calculator = new Calculate();
             int status = 0;
+            Calculate calculator = new Calculate();
+            calculator.Start(0);
 
             while (status != 1){
 
                 Console.WriteLine("--------------------------------------------------");
+                status = 1;
 
-                Console.WriteLine("+) Addition\n-) Subtraction\n*) Multiplication\n/) Division\n!) Quit");
-                string input_string = Console.ReadLine();
-                Console.WriteLine("Which numbers would you like to " + input_string + " (EX:1 2):");
-                string numbers = Console.ReadLine();
-
-                try
-                {
-                    string[] numbersList = numbers.Split();
-                    float one = float.Parse(numbersList[0]);
-                    float two = float.Parse(numbersList[1]);
-                    char sym = input_string[0];
-                    float total = 0;
-
-                if (sym == '+'){
-                    total = calculator.add(one, two);
-                    Console.WriteLine($"The answer is: {total}");
-                }
-                else if(sym == '-'){
-                    total = calculator.sub(one, two);
-                    Console.WriteLine($"The answer is: {total}");
-                }
-                else if(sym == '*'){
-                    total = calculator.mult(one, two);
-                    Console.WriteLine($"The answer is: {total}");
-                }
-                else if(sym == '/'){
-                    total = calculator.div(one, two);
-                    Console.WriteLine($"The answer is: {total}");
-                }
-                else if(sym == '!'){
-                    Console.WriteLine("Shutting down...");
-                    status = 1;
-                }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error: {ex.Message}");
-                }
             }
+        }
+
+        public void Start(float total){
+
+            Calculate calculator = new Calculate();
+
+            Console.WriteLine("Welcome! What would you like to solve?");
+            Console.WriteLine("+) Addition\n-) Subtraction\n*) Multiplication\n/) Division\n!) Quit");
+            string input_string = Console.ReadLine();
+            Console.WriteLine("Which numbers would you like to " + input_string + " (EX:1 2):");
+            string numbers = Console.ReadLine();
+            total = calculator.solve(total, numbers, input_string);
+            Console.WriteLine($"The answer is: {total}");
         }
 
         public void repeat(float total){
 
-            int status = 0;
+            Calculate calculator = new Calculate();
 
             Console.WriteLine("+) Addition\n-) Subtraction\n*) Multiplication\n/) Division\n!) Quit");
             string input_string = Console.ReadLine();
             Console.WriteLine($"Which number would you like to {input_string} to your total ({total}):");
             string numbers = Console.ReadLine();
+            total = calculator.solve(total, numbers, input_string);
+            Console.WriteLine($"The answer is: {total}");
+        }
+
+        public float solve(float total, string numbers, string input_string){
+            
+            int status = 0;
             Calculate calculator = new Calculate();
 
             try
-                {
-                    string[] numbersList = numbers.Split();
-                    float one = float.Parse(numbersList[0]);
-                    float two = float.Parse(numbersList[1]);
-                    char sym = input_string[0];
+            {
+                string[] numbersList = numbers.Split();
+                float one = float.Parse(numbersList[0]);
+                float two = float.Parse(numbersList[1]);
+                char sym = input_string[0];
 
                 if (sym == '+'){
                     total = calculator.add(one, two);
-                    Console.WriteLine($"The answer is: {total}");
+                    return total;
                 }
                 else if(sym == '-'){
                     total = calculator.sub(one, two);
-                    Console.WriteLine($"The answer is: {total}");
+                    return total;
                 }
                 else if(sym == '*'){
                     total = calculator.mult(one, two);
-                    Console.WriteLine($"The answer is: {total}");
+                    return total;
                 }
                 else if(sym == '/'){
                     total = calculator.div(one, two);
-                    Console.WriteLine($"The answer is: {total}");
+                    return total;
                 }
                 else if(sym == '!'){
                     Console.WriteLine("Shutting down...");
-                    status = 1;
+                    return status;
                 }
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine($"Error: {ex.Message}");
                 }
+                
+                return status;
         }
 
         public float add(float n1, float n2){
